4e381b40304d7b3a16d7f99953327e6d47cd594f
Pabl0Parra <pabsblue16@gmail.com>
2022-06-15 22:38:28 +0200

    finished ex_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { services } from "./components/services";
+import "./index.css";
 
-function App() {
+const getFormattedPrice = (price) => `$${price.toFixed(2)}`;
+
+export default function App() {
+  const [checkedState, setCheckedState] = useState(
+    new Array(services.length).fill(false)
+  );
+
+  console.log(checkedState);
+
+  const [total, setTotal] = useState(0);
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, id) =>
+      id === position ? !item : item
+    );
+
+    setCheckedState((prev) => (prev = updatedCheckedState));
+
+    const totalPrice = updatedCheckedState.reduce((sum, currentState, id) => {
+      if (currentState === true) {
+        return sum + services[id].price;
+      }
+      return sum;
+    }, 0);
+
+    setTotal((prev) => (prev = totalPrice));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Which services do you require?</h3>
+      <div className="services-list">
+        {services.map(({ name, price }, id) => {
+          return (
+            <div key={id}>
+              <div className="services-list-item">
+                <input
+                  type="checkbox"
+                  id={`custom-checkbox-${id}`}
+                  name={name}
+                  value={name}
+                  checked={checkedState[id]}
+                  onChange={() => handleOnChange(id)}
+                />
+                <label htmlFor={`custom-checkbox-${id}`}>{name}</label>
+                <span className="price-section">
+                  {getFormattedPrice(price)}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+
+        <div className="services-total-price">
+          <h4>
+            <strong>Total: {getFormattedPrice(total)}</strong>
+          </h4>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/services.js
@@ -0,0 +1,5 @@
+export const services = [
+  { id: 1, name: "Website design", price: 500 },
+  { id: 2, name: "SEO Campaign", price: 300 },
+  { id: 3, name: "Google Ads Campaign", price: 200 },
+];
