7192fa87475ed08c5f06a2407095ce24b4aca8fc
Pabl0Parra <98957023+Pabl0Parra@users.noreply.github.com>
2022-06-16 10:36:16 +0200

    added comments to ex_1

--- src/App.js
+++ src/App.js
@@ -2,34 +2,39 @@ import { useState } from "react";
 import { services } from "./components/services";
 import "./index.css";
 
+// --------------------------------------- LOGIC -------------------------------------------------------
+
 const getFormattedPrice = (price) => `$${price.toFixed(2)}`;
 
 export default function App() {
+  // create an [] to store the initial state (unchecked = false) of the checkboxes
   const [checkedState, setCheckedState] = useState(
     new Array(services.length).fill(false)
   );
 
   console.log(checkedState);
-
+  // useState for calculated total budget
   const [total, setTotal] = useState(0);
-
+  // every time we click a checkbox, the state updates to true if the conditions occur, otherwise...remains false
   const handleOnChange = (position) => {
+    // we mapped checkedState[] & if position value is equal to the current id, then we modify its value (from false to true), otherwise it remains false. This allows me to check or uncheck boxes.
     const updatedCheckedState = checkedState.map((item, id) =>
       id === position ? !item : item
     );
 
     setCheckedState((prev) => (prev = updatedCheckedState));
-
+    // setCheckedState tends to update asyncronally, so it doesn´t guarantee us that we will get the updated value in the next line
+    // so I created a new variable to apply the reduce() method to it
     const totalPrice = updatedCheckedState.reduce((sum, currentState, id) => {
       if (currentState === true) {
         return sum + services[id].price;
       }
       return sum;
     }, 0);
-
+    // applied above variable to setTotal to get the final price
     setTotal((prev) => (prev = totalPrice));
   };
-
+  // ------------------------------------------------ RENDERING ----------------------------------------------
   return (
     <div className="App">
       <h3>Which services do you require?</h3>
@@ -43,7 +48,9 @@ export default function App() {
                   id={`custom-checkbox-${id}`}
                   name={name}
                   value={name}
+                  // this attribute can be either true or false, depending on "checkState" current state
                   checked={checkedState[id]}
+                  // added this () so that it changes the state when is clicked or unclicked by handleOnChange
                   onChange={() => handleOnChange(id)}
                 />
                 <label htmlFor={`custom-checkbox-${id}`}>{name}</label>
